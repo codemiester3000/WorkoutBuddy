939db8b4bfd0a356a9cddf3d6d14b37806bf19d9
Owen Khoury <owenkhoury@google.com>
2024-05-07 20:24:04 -0400

    Initial Commit

---
+++ AI Health Assistant/AI_Health_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Health_AssistantApp.swift
+//  AI Health Assistant
+//
+//  Created by Owen Khoury on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AI_Health_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI Health Assistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI Health Assistant
+//
+//  Created by Owen Khoury on 5/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
